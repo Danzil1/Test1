d4ef6f80b52e1df895cf02d1393bd5f4a0ef2c05
Danzil1 <danzil.fernandes@sjinnovation.com>
2020-05-22 11:38:57 +0530

    Python file added

---
+++ testOne.py
@@ -0,0 +1,6 @@
+for x in range(6):
+    print("Welcome to Jenkins github Integration " + str(x))
+else:
+    print("Yes. You successfully completed the first build.")
+    print("You completed more than 50%. Great job")
+    print("Testing for crons on deployment")
